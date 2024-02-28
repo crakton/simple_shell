acf2099d0184b9a75e9ff9bfba84ef45e2f13b2b
crakton <redemptionjonathan1@gmail.com>
2024-02-29 00:54:11 +0100

    some warnings on the main.c file

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	ssize_t _b = 0;
 	size_t _s = 0;
-	char *e = NULL, *args[20];
+	char *e = NULL, *a[20];
 	int i = 1, v = 0, ex = 0, _e = 0, b = 0;
 
 	_prompt("$ ", 2);
@@ -16,24 +16,24 @@ int main(void)
 	{
 		if (*e != '\n')
 		{
-			cmds(e, args);
-			if (args[0] != NULL)
+			cmds(e, a);
+			if (a[0] != NULL)
 			{
-				ex = verify(args[0]);
+				ex = verify(a[0]);
 				if (ex != 0)
 				{
-					v = env_path(args);
+					v = env_path(a);
 					if (v == 0)
-						_e = exec(args), free(*args);
+						_e = exec(a), free(*a);
 					else
 					{
-						b = builtins(args, ex);
+						b = builtins(a, ex);
 						if (b != 0)
-							_e = err_print(args, i), free(e);
+						_e = err_print(a, i), free(e);
 					}
 				}
 				else
-					_e = exec(args), free(e);
+					_e = exec(a), free(e);
 			}
 			else
 				free(e);
