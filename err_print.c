7bd4363b6a76a85381c8ae7c4adbfb6e64738d77
crakton <redemptionjonathan1@gmail.com>
2024-02-29 00:23:00 +0100

    error prompt statement corrected

--- err_print.c
+++ err_print.c
@@ -17,11 +17,10 @@ int err_print(char **args, int counter)
 		write(2, mode1, 3);
 	else
 		write(2, mode2, 5);
-	write(2, ":(", 2);
+	write(2, ": ", 2);
 	err_code(counter);
-	write(2, ") ", 2);
 	write(2, args[0], strlen(args[0]));
-	write(2, ":: not found", 12);
+	write(2, ": not found", 12);
 	write(2, "\n", 2);
 	return (127);
 }
